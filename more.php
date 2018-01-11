ebbfd7616d6fafa552586c0df759ffe6b05802c6
loracy <lora860227@gmail.com>
2018-01-11 20:05:06 +0800

    01-11

--- more.php
+++ more.php
@@ -3,68 +3,96 @@
 ?>
 
 <div class="container content pt-5 pb-5">
-    <div class="article-1 pb-5 mb-5">
-        <div class="text-center mb-5">
+    <div class="more-article pb-5 mb-5">
+        <div class="text-center mb-5 p-2">
             <span class="text_slogan">
                 關於營養製劑
             </span>
         </div>
-        <!-- slogan -->
-
-        <!-- <div class="media offset-md-1 col-sm-10 p-0"> -->
-
-            <!-- <img class="mr-4 col-sm-6 p-1" src="assets/main-img.png" alt=""> -->
-
-        <div class="text_description col-sm-8 offset-sm-2 p-1 ">
-            營養製劑(Nutraceutical)是1980年以後才發展出來的新觀念，Nutraceutical= Nutrition(營養基礎)+ Pharmaceutical(類似藥物的開發流程)，具有醫學、生理學等明確成分－原創性研究經過臨床明確的科學數據證明其功效及安全性並具創新研究專利可以有效預防疾病及維持健康的食品。
+        <div class="text-center col-sm-8 offset-sm-2 bg-card-1">
+            <span class="text_description">
+                營養製劑(Nutraceutical)是1980年以後才發展出來的新觀念，Nutraceutical= Nutrition(營養基礎)+ Pharmaceutical(類似藥物的開發流程)，具有醫學、生理學等明確成分－原創性研究經過臨床明確的科學數據證明其功效及安全性並具創新研究專利可以有效預防疾病及維持健康的食品。
+            </span>
         </div>
     </div>
-    <!-- article-1 -->
+    <!-- article：關於營養劑 -->
 
-    <div class="article-2">
-        <div class="text-center mt-5 mb-5">
+    <div class="more-article">
+        <div class="text-center mt-5 mb-5 p-2">
             <span class="text_slogan">
                 我們怎麼具備營養製劑的條件？
             </span>
         </div>
-        <!-- slogan -->
-
-        <!-- <div class="media offset-md-1 col-sm-10 p-0"> -->
-
-            <!-- <img class="mr-4 col-sm-6 p-1" src="assets/main-img.png" alt=""> -->
-        <div class="mt-5 mb-5">
-            <div class="text_subtitle col-sm-8 offset-sm-2 p-1">
-                品牌原料的概念
+        <div class="row">
+            <div class="mb-5 col-sm-10 offset-sm-1 bg-card-1 row">
+                <div class="p-2 col-sm-4">
+                    <span class="text_number">
+                        1
+                    </span>     
+                    <span class="text_subtitle">
+                        品牌原料的概念
+                    </span>
+                </div>
+                <div class="divider"></div>
+                <div class="ml-5 p-2 col-sm-6">
+                    <span class="text_description">
+                        開發一個新的營養製劑產品，需要投入相當的時間金錢及人力、物力是原創性的研究開發，且對其機轉跟生物利用率上會有明確的結果，發表生產的技術及其應用會受到一定程度的專利保護無法任意模仿。
+                    </span>
+                </div>
             </div>
-            <div class="text_description col-sm-8 offset-sm-2 p-1 ">
-                開發一個新的營養製劑產品，需要投入相當的時間金錢及人力、物力是原創性的研究開發，且對其機轉跟生物利用率上會有明確的結果，發表生產的技術及其應用會受到一定程度的專利保護無法任意模仿。
+            <!-- 1-品牌原料的概念 -->
+            <div class="mb-5 col-sm-10 offset-sm-1 bg-card-1 row">
+                <div class="p-2 col-sm-4">
+                    <span class="text_number">
+                        2
+                    </span>     
+                    <span class="text_subtitle">
+                        臨床試驗及定性定量的概念
+                    </span>
+                </div>
+                <div class="divider"></div>
+                <div class="ml-5 p-2 col-sm-6">
+                    <span class="text_description">
+                        明確的有效成分經過一定程度的臨床測驗後的成果，能清楚地知道其功效怎麼用、用多少及用在什麼狀況下會有什麼結果。
+                    </span>
+                </div>
             </div>
+            <!-- 2-臨床試驗及定性定量的概念 -->
+            <div class="mb-5 col-sm-10 offset-sm-1 bg-card-1 row">
+                <div class="p-2 col-sm-4">
+                    <span class="text_number">
+                        3
+                    </span>     
+                    <span class="text_subtitle">
+                        相關單位的認證
+                    </span>
+                </div>
+                <div class="divider"></div>
+                <div class="ml-5 p-2 col-sm-6">
+                    <span class="text_description">
+                        營養製劑通常都會有許多功效、成分結構或安全性上的認證，這是在多方面且深入的研究後在商業化前應有的過程，例如：GRAS（安全食品認證） Non-Allergenic（不引起過敏反應）、GMO-free（無基因改良成份食品）、Kosher（猶太教認證）Halal（回教認證）。
+                    </span>
+                </div>
+            </div>
+            <!-- 3-相關單位的認證 -->
+            <div class="mb-5 col-sm-10 offset-sm-1 bg-card-1 row">
+                <div class="p-2 col-sm-4">
+                    <span class="text_number">
+                        4
+                    </span>     
+                    <span class="text_subtitle">
+                        資訊的透明化
+                    </span>
+                </div>
+                <div class="divider"></div>
+                <div class="ml-5 p-2 col-sm-6">
+                    <span class="text_description">
+                        經過一定的研發過程及成果發表的品牌原料，使用者通常都能容易地透過公開的訊息取得想要瞭解的資訊來檢視其真實性和安全性，產品外包裝上也都會有原料的品牌名稱或開發商授權使用的註冊商標。
+                    </span>
+                </div>
+            </div>
+            <!-- 4-資訊的透明化 -->
         </div>
-        <div class="mt-5 mb-5">
-            <div class="text_subtitle col-sm-8 offset-sm-2 p-1">
-                臨床試驗及定性定量的概念
-            </div>
-            <div class="text_description col-sm-8 offset-sm-2 p-1 ">
-                明確的有效成分經過一定程度的臨床測驗後的成果，能清楚地知道其功效怎麼用、用多少及用在什麼狀況下會有什麼結果。
-            </div>
-        </div>
-        <div class="mt-5 mb-5">
-            <div class="text_subtitle col-sm-8 offset-sm-2 p-1">
-                相關單位的認證
-            </div>
-            <div class="text_description col-sm-8 offset-sm-2 p-1 ">
-                營養製劑通常都會有許多功效、成分結構或安全性上的認證，這是在多方面且深入的研究後在商業化前應有的過程，例如：GRAS（安全食品認證） Non-Allergenic（不引起過敏反應）、GMO-free（無基因改良成份食品）、Kosher（猶太教認證）Halal（回教認證）。
-            </div>
-        </div>
-        <div class="mt-5 mb-5">
-            <div class="text_subtitle col-sm-8 offset-sm-2 p-1">
-                資訊的透明化
-            </div>
-            <div class="text_description col-sm-8 offset-sm-2 p-1 ">
-                經過一定的研發過程及成果發表的品牌原料，使用者通常都能容易地透過公開的訊息取得想要瞭解的資訊來檢視其真實性和安全性，產品外包裝上也都會有原料的品牌名稱或開發商授權使用的註冊商標。
-            </div>
-        </div>
-        
             
         <!-- </div> -->
         <!-- media -->
