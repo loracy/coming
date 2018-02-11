6d3de31d0703df3c75d7c9994768d3304261b2bc
loracy <lora860227@gmail.com>
2018-02-12 00:59:36 +0800

    180212

--- js/navbar.js
+++ js/navbar.js
@@ -1,13 +1,18 @@
-if( location.pathname == '/cict105/loracy/more.php'){
-    $('.nav-item-more div').addClass('active');
+if( location.pathname == '/coming/'){
+    // $('.nav-item-more div').addClass('active');
     console.log( 'path = ' + location.pathname );
-}else if( location.pathname == '/cict105/loracy/contact.php'){
-    $('.nav-item-contact div').addClass('active');
+}else if( location.pathname == '/coming/contact.php'){
+    // $('.nav-item-contact div').addClass('active');
     console.log( 'path = ' + location.pathname );
-}else if( location.pathname == '/cict105/loracy/product.php'){
-    $('.nav-item-product div').addClass('active');
+}else if( location.pathname == '/coming/product.php'){
+    // $('.nav-item-product div').addClass('active');
+    console.log( 'path = ' + location.pathname );
+}else if(location.pathname == '/coming/detail.php'){
+    console.log( 'path = ' + location.pathname );
+}else if(location.pathname == '/coming/more.php'){
     console.log( 'path = ' + location.pathname );
 }else{
     console.log('there is wrong path');
+    console.log( 'path = ' + location.pathname );
 
 }
--- product.php
+++ product.php
@@ -2,17 +2,20 @@
 	include ('header.php');
 ?>
 
-<div class="container content pt-sm-5 pt-3 pb-5">
-    <div class="col-12 col-md-5 ml-md-5 ml-auto mb-3 row bgWhite cardStyle productCard">
-        <span class="col-md-6 productImage">
-            <img style="width=100%;height:100%;" src="./assets/product-1.png" alt="產品-1">
-        </span>
-        <!-- productImage -->
-        <span class="col-md-6 productText">
-            <div class="mb-3 mt-2 productTitle">
-                永保康
+<div class="container content pt-3 pb-5 pt-sm-5 d-flex">
+    <div class="col-12 col-md-5 mb-3 p-0 row bgWhite cardStyle productCard">
+        <div id="cardDec"></div>
+        <div class="col-6 p-0 productCardLeft">
+            <div class="productName">
+                <img src="./icons/coming.svg"/>
+                <h4>永保康</h4>
             </div>
-            <ul>
+            <div class="productImage">
+                <img src="./assets/product-1.png" alt="產品-1">
+            </div>
+        </div>
+        <div class="col-6 productCardRight">
+            <ul class="productList">
                 <li class="listItem">
                     <img class="mr-2" src="./icons/check.svg">
                     <p>純天然植物提煉</p>
@@ -30,69 +33,62 @@
                     <p>獲得多項世界專利</p>
                 </li>
             </ul>
-        </span>
-        <!-- productText  -->
-        <a class="btn btn-info btn-info-detail" href="./detail.php" role="button" aria-pressed="true">
-            <svg xmlns="http://www.w3.org/2000/svg" 
-                width="24" height="24" viewBox="0 0 24 24" 
-                fill="none" stroke="#ffffff" 
-                stroke-width="3" stroke-linecap="round" stroke-linejoin="round" 
-                class="feather feather-arrow-right">
-                <line x1="4" y1="12" x2="20" y2="12"></line>
-                <polyline points="14 6 20 12 14 18"></polyline>
-            </svg>
-        </a><!-- btn-info -->
-        <div id="cardDec"></div>
+        </div>
+        <!-- <a class="btn btn-info" href="./detail.php" role="button" aria-pressed="true"> -->
+        <?php 
+            $productItem = 'product-1';
+            $name = '永保康';
+            $intro = '純天然植物提煉、紐西蘭進口';
+        ?>
+        <a class="btn btn-info" href="<?php echo "./detail.php?product=".$productItem."&name=".$name."&intro=".$intro ?>" role="button" aria-pressed="true">
+            <p>瞭解更多</p>
+            <img src="./icons/arrow-right.svg"/>
+        </a>
     </div>
     <!-- 產品一 -->
-
-    <div class="col-md-5 ml-md-5 col-12 ml-auto mb-3 row bgWhite cardStyle productCard">
-        <span class="col-md-6 productImage">
-            <img style="width=100%;height:100%;" src="./assets/product-2.png" alt="產品-2">
-        </span>
-        <!-- productImage -->
-        <span class="col-md-6 productText">
-            <div class="mb-3 mt-2 productTitle">
-                舌下錠
+    <div class="col-12 col-md-5 mb-3 p-0 row bgWhite cardStyle productCard">
+        <div id="cardDec"></div>
+        <div class="col-6 p-0 productCardLeft">
+            <div class="productName">
+                <img src="./icons/coming.svg"/>
+                <h4>高一等</h4>
             </div>
-            <ul>
+            <div class="productImage">
+                <img src="./assets/product-1.png" alt="產品-1">
+            </div>
+        </div>
+        <div class="col-6 productCardRight">
+            <ul class="productList">
                 <li class="listItem">
                     <img class="mr-2" src="./icons/check.svg">
-                    <p>德國原裝進口</p>
+                    <p>名字很好聽</p>
                 </li>
                 <li class="listItem">
                     <img class="mr-2" src="./icons/check.svg">
-                    <p>獨特舌下口含錠</p>
+                    <p>符合ABC檢驗</p>
                 </li>
                 <li class="listItem">
                     <img class="mr-2" src="./icons/check.svg">
-                    <p>快速吸收</p>
+                    <p>讓你更有信心</p>
                 </li>
                 <li class="listItem">
                     <img class="mr-2" src="./icons/check.svg">
-                    <p>眾多臨床專利</p>
-                </li>
-                <li class="listItem">
-                    <img class="mr-2" src="./icons/check.svg">
-                    <p>實驗數據佐證</p>
+                    <p>我弟本身有在吃</p>
                 </li>
             </ul>
-        </span>
-        <!-- productText  -->
-        <a class="btn btn-info btn-info-detail" href="./detail.php" role="button" aria-pressed="true">
-            <svg xmlns="http://www.w3.org/2000/svg" 
-                width="24" height="24" viewBox="0 0 24 24" 
-                fill="none" stroke="#ffffff" 
-                stroke-width="3" stroke-linecap="round" stroke-linejoin="round" 
-                class="feather feather-arrow-right">
-                <line x1="4" y1="12" x2="20" y2="12"></line>
-                <polyline points="14 6 20 12 14 18"></polyline>
-            </svg>
+        </div>
+        <!-- <a class="btn btn-info" href="./detail.php" role="button" aria-pressed="true"> -->
+        <?php 
+            $productItem = 'product-2';
+            $name = '高一等';
+            $intro = '所有矮冬瓜的救星';
+        ?>
+        <a class="btn btn-info" href="<?php echo "./detail.php?product=".$productItem."&name=".$name."&intro=".$intro ?>" role="button" aria-pressed="true">
+            <p>瞭解更多</p>
+            <img src="./icons/arrow-right.svg"/>
         </a>
-        <!-- btn-info -->
-        <div id="cardDec"></div>
     </div>
-    <!-- 產品二 -->
+    <!-- 產品一 -->
 
 </div>
   </body>
