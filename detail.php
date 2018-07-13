c8db6139df758d1f0a003477ef47e5a66ce27d6a
loracy <lora860227@gmail.com>
2018-07-13 13:48:46 +0800

    完成product.php架構＆第一版圖片

--- detail.php
+++ detail.php
@@ -7,17 +7,17 @@
         <nav class="d-none d-sm-flex col-10 offset-1" aria-label="breadcrumb">
             <ol class="breadcrumb">
                 <li class="breadcrumb-item"><a href="product.php">產品中心</a></li>
-                <li class="breadcrumb-item active" aria-current="page"><?php echo $_GET['name'];?></li>
+                <li class="breadcrumb-item active" aria-current="page">產品名稱</li>
             </ol>
         </nav>
         <div class="col-12 col-sm-10 offset-sm-1 d-flex detailSection">
             <div class="col-12 col-sm-5">
-                <img class="detailImage" src="<?php echo "./assets/".$_GET['product']."-lg.png"?>" alt="產品-1">
+                <img class="detailImage" src="<?php echo "./assets/product-".$_GET['product'].".png"?>" alt="產品圖片">
             </div>
             <div class="col-12 col-sm-7">
                 <div class="d-flex mb-sm-4 detailName">
-                    <h3><?php echo $_GET['name'];?></h3>
-                    <p><?php echo $_GET['intro'];?></p>
+                    <h3>產品名稱</h3>
+                    <p>特色</p>
                     <a class="d-none d-sm-flex btn btn-cta btn-lg btn-contact" href="./contact.php" role="button" aria-pressed="true" data-toggle="modal" data-target=".modal-lg">聯繫我們</a>
                     <a class="d-flex d-sm-none btn btn-cta btn-contact-sm" href="./contact.php" role="button" aria-pressed="true"><img src="./icons/contact.svg"/></a>
                 </div>
@@ -71,40 +71,26 @@
 
     function showData(jsonObj) {
         var productItem = "<?php echo $_GET['product']; ?>";
-        console.log('productItem = ' + productItem);
-        switch ( productItem ) {
-            case 'product-1':
-                var data = jsonObj['detail-1'];
-                break;
-            case 'product-2':
-                var data = jsonObj['detail-2'];
-                break;
-            case 'product-3':
-                var data = jsonObj['detail-3'];
-                break;
-            case 'product-4':
-                var data = jsonObj['detail-4'];
-                break;
-            default:
-                break;
-        }
-        for(i=1;i<4;i++){
-            document.getElementById("detailContentTitle-"+i).innerHTML=data[i-1]['title'];
-            document.getElementById("detailContentIntro-"+i).innerHTML=data[i-1]['content'];
-        }
+        console.log('product = ' + productItem);
+        data = jsonObj['products'];
+        console.log(data[0]['detail']);
+        // for(i=1;i<4;i++){
+        //     document.getElementById("detailContentTitle-"+i).innerHTML=data[i-1]['title'];
+        //     document.getElementById("detailContentIntro-"+i).innerHTML=data[i-1]['content'];
+        // }
         // document.getElementById("detailContentTitle").innerHTML=data[0]['title'];
         // document.getElementById("detailContentIntro").innerHTML=data[0]['content'];
     }
 
 </script>
 <script>
-    console.log(document.body.offsetHeight);
-    console.log(document.body.clientHeight);
-    console.log(window.screen.height);
-    console.log(window.screen.availHeight);
-    $(document).ready(function(){
+    // console.log(document.body.offsetHeight);
+    // console.log(document.body.clientHeight);
+    // console.log(window.screen.height);
+    // console.log(window.screen.availHeight);
+    // $(document).ready(function(){
 
-    });
+    // });
     
 </script>
 
--- product.php
+++ product.php
@@ -2,6 +2,35 @@
 	include ('header.php');
 ?>
 
+<script type="text/javascript">  
+// 取得json資料
+var requestURL = 'https://loracy.github.io/coming/assets/details.json';
+var request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+request.onload = function() {
+    var jsonObj = request.response;
+    showData(jsonObj);
+}
+
+function showData(jsonObj) {
+    // data is a array-object that includes all product page information
+    data = jsonObj['products'];
+    // render the product's name
+    for( i=1; i <= data.length ; i++ ){
+        document.getElementsByClassName('productName-' + i)[0].innerText = data[i-1]['name'];
+        document.getElementsByClassName('productName-' + i)[1].innerText = data[i-1]['name'];
+    }
+    for( i=1; i <= data.length ; i++ ){
+        console.log(data[i-1]);
+        for( j=1; j <= 4 ; j++ ){
+            document.getElementsByClassName('productFeature-' + i)[j-1].innerText = data[i-1]['feature'][j];
+        }
+    }
+}
+
+</script>
 <?php 
     $productItem = 'product-1';
     $name = array('高一等', '晶盈明亮口含錠', '滴丸', '強康', '松樹皮3', '蘑菇D2');
@@ -11,13 +40,14 @@
 
 <div class="container content pt-5 pb-5">
     <div class="row">
+    <!-- get all the products card -->
         <?php for ( $i=1 ; $i<=$productLength ; $i++ ){ ?>
         <div class="col-12 col-sm-5 mb-5 p-0 row bgWhite cardStyle productCard">
             <div id="cardDec"></div>
             <div class="col-12 col-sm-6 p-0 productCardLeft">
                 <div class="productName d-flex d-sm-none">
                     <img src="./icons/coming.svg"/>
-                    <h4><?php echo $name[$i-1] ?></h4>
+                    <h4 class="productName-<?php echo $i ?>"></h4>
                 </div>
                 <div class="productImage">
                     <img src="<?php echo "./assets/product-".$i.".png" ?>">
@@ -25,28 +55,19 @@
             </div>
             <div class="col-12 col-sm-6 productCardRight">
                 <div class="productName d-none d-sm-flex">
-                    <h4><?php echo $name[$i-1] ?></h4>
+                    <h4 class="productName-<?php echo $i ?>"></h4>
                 </div>
                 <ul class="productList">
-                    <li class="listItem">
-                        <img class="mr-2" src="./icons/check.svg">
-                        <p>純天然植物提煉</p>
+                    <!-- get 4 features of the product -->
+                    <?php for ( $j=1 ; $j<=4 ; $j++ ){ ?>
+                    <li class="listItem d-flex align-items-start my-1">
+                        <img class="mr-2 mt-1" src="./icons/check.svg">
+                        <p class="productFeature-<?php echo $i ?>"></p>
                     </li>
-                    <li class="listItem">
-                        <img class="mr-2" src="./icons/check.svg">
-                        <p>紐西蘭進口</p>
-                    </li>
-                    <li class="listItem">
-                        <img class="mr-2" src="./icons/check.svg">
-                        <p>營養製劑化</p>
-                    </li>
-                    <li class="listItem">
-                        <img class="mr-2" src="./icons/check.svg">
-                        <p>獲得多項世界專利</p>
-                    </li>
+                    <?php } ?>
                 </ul>
             </div>
-            <a class="btn btn-info" href="<?php echo "./detail.php?product=product-".$i."&name=".$name[$i-1]."&intro=".$intro ?>" role="button" aria-pressed="true">
+            <a class="btn btn-info" href="<?php echo "./detail.php?product=".$i ?>" role="button" aria-pressed="true">
                 <p class="d-none d-sm-block">瞭解更多</p>
                 <img src="./icons/arrow-right.svg"/>
             </a>
