9fc82755bf8ac57b641f20ca53d78da5c813300d
loracy <lora860227@gmail.com>
2018-07-17 00:50:03 +0800

    add footer & product RWD

---
+++ footer.php
@@ -0,0 +1,62 @@
+
+<script type="text/javascript">  
+// 取得json資料
+var requestURL = 'https://loracy.github.io/coming/assets/details.json';
+var request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+request.onload = function() {
+    var jsonObj = request.response;
+    showData(jsonObj);
+}
+
+function showData(jsonObj) {
+    // data is a array-object that includes all product page information
+    data = jsonObj['products'];
+    // render the product's name
+    for( i=1; i <= data.length ; i++ ){
+        document.getElementsByClassName('footerProductContentRowLink-' + i)[0].innerText = data[i-1]['name'];
+    }
+}
+
+</script>
+<div class="footer" style="padding:0px;">
+    <div class="container-fluid pt-5">
+        <div class="row pb-5">
+            <div class="col-12 col-sm-4 d-flex align-items-center justify-content-center footerLogo">
+                <img src="./assets/logo.jpg" width="auto" height="49px" alt="logo">
+            </div>
+            <div class="col-12 col-sm-3 mt-4 mt-sm-0 footerProduct">
+                <h5 class="align-items-center">產品中心</h5>
+                <div class="d-flex flex-column align-items-center align-items-start footerProductContent">
+                    <?php for ( $i=1 ; $i<=2 ; $i++ ){ ?>
+                    <div class="d-flex flex-row footerProductContentRow">
+                        <?php for ( $j=1 ; $j<=3 ; $j++ ){ ?>
+                            <a href="<?php echo "./detail.php?product=".($j+($i-1)*3) ?>" class="footerProductContentRowLink-<?php echo $j+($i-1)*3 ?>"></a>
+                        <?php } ?>
+                    </div>
+                    <?php } ?>
+                </div>
+            </div>
+            <div class="col-12 col-sm-4 mt-4 mt-sm-0 footerContact">
+                <h5 class="align-items-center">聯絡資訊</h5>
+                <div class="d-flex flex-column align-items-center footerContactContent">
+                    <p>聯絡地址：台中市西區華美西街一段82號</p>
+                    <p>市話：04-23101377 /<br class="d-block d-sm-none">
+                    免費專線：0800-089595 / <br class="d-block d-sm-none">
+                    傳真：04-23101335</p>
+                    <p>電子郵件：saray560505@gmail.com</p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 py-4 d-flex align-items-center justify-content-center footerPolicy">
+                <h5>版權所有 - © 2018 康盈生技有限公司</h5>
+            </div>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
--- product.php
+++ product.php
@@ -39,41 +39,45 @@ function showData(jsonObj) {
 ?>
 
 <div class="container content pt-5 pb-5">
-    <div class="row">
+    <div class="row px-3 p-sm-0">
     <!-- get all the products card -->
         <?php for ( $i=1 ; $i<=$productLength ; $i++ ){ ?>
-        <div class="col-12 col-sm-5 mb-5 p-0 row bgWhite cardStyle productCard">
-            <div id="cardDec"></div>
-            <div class="col-12 col-sm-6 p-0 productCardLeft">
-                <div class="productName d-flex d-sm-none">
-                    <img src="./icons/coming.svg"/>
-                    <h4 class="productName-<?php echo $i ?>"></h4>
+        <div class="col-12 col-sm-6 col-lg-5 mb-5 p-0 bgWhite cardStyle productCard">
+            <div class="row m-0">
+                <div id="cardDec"></div>
+                <div class="col-12 col-sm-6 p-0 productCardLeft">
+                    <div class="productName d-flex d-sm-none">
+                        <img src="./icons/coming.svg"/>
+                        <h4 class="productName-<?php echo $i ?>"></h4>
+                    </div>
+                    <div class="productImage">
+                        <img src="<?php echo "./assets/product-".$i.".png" ?>">
+                    </div>
                 </div>
-                <div class="productImage">
-                    <img src="<?php echo "./assets/product-".$i.".png" ?>">
+                <div class="col-12 col-sm-6 productCardRight">
+                    <div class="productName d-none d-sm-flex">
+                        <h4 class="productName-<?php echo $i ?>"></h4>
+                    </div>
+                    <ul class="productList">
+                        <!-- get 4 features of the product -->
+                        <?php for ( $j=1 ; $j<=4 ; $j++ ){ ?>
+                        <li class="listItem d-flex align-items-start my-1">
+                            <img class="mr-2 mt-1" src="./icons/check.svg">
+                            <p class="productFeature-<?php echo $i ?>"></p>
+                        </li>
+                        <?php } ?>
+                    </ul>
                 </div>
+                <a class="btn btn-info" href="<?php echo "./detail.php?product=".$i ?>" role="button" aria-pressed="true">
+                    <p class="d-none d-sm-block">瞭解更多</p>
+                    <img src="./icons/arrow-right.svg"/>
+                </a>
             </div>
-            <div class="col-12 col-sm-6 productCardRight">
-                <div class="productName d-none d-sm-flex">
-                    <h4 class="productName-<?php echo $i ?>"></h4>
-                </div>
-                <ul class="productList">
-                    <!-- get 4 features of the product -->
-                    <?php for ( $j=1 ; $j<=4 ; $j++ ){ ?>
-                    <li class="listItem d-flex align-items-start my-1">
-                        <img class="mr-2 mt-1" src="./icons/check.svg">
-                        <p class="productFeature-<?php echo $i ?>"></p>
-                    </li>
-                    <?php } ?>
-                </ul>
-            </div>
-            <a class="btn btn-info" href="<?php echo "./detail.php?product=".$i ?>" role="button" aria-pressed="true">
-                <p class="d-none d-sm-block">瞭解更多</p>
-                <img src="./icons/arrow-right.svg"/>
-            </a>
         </div>
         <?php } ?>
     </div>
 </div>
-  </body>
-</html>
+
+<?php 
+	include ('footer.php');
+?>
